18e69b49f946919bf6cb43b615fcd499c1bc65a3
devbrock <tbrockb@gmail.com>
2019-06-11 09:57:35 -0500

    Add environment variables

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,28 +1,29 @@
 import ApolloClient from 'apollo-boost';
+import config from '../config';
 
 export const client = new ApolloClient({
-  // uri: 'http://dev.relatemediadesign.com:4000/graphql',
-  uri: 'http://localhost:4000/graphql',
-  credentials: 'include',
-  onError: ({ graphQLErrors = false, networkError, response }) => {
-    if (graphQLErrors) {
-      console.log('ApolloClient graphQLErrors');
-      console.log(graphQLErrors);
+	uri: config.graphqlEndpoint,
+	// uri: 'http://localhost:4000/graphql',
+	credentials: 'include',
+	onError: ({ graphQLErrors = false, networkError, response }) => {
+		if (graphQLErrors) {
+			console.log('ApolloClient graphQLErrors');
+			console.log(graphQLErrors);
 
-      const notAuthenticated = graphQLErrors.find(err => {
-        return err.message === 'Not authenticated!';
-      });
+			const notAuthenticated = graphQLErrors.find(err => {
+				return err.message === 'Not authenticated!';
+			});
 
-      if (notAuthenticated) {
-        console.log('NOT AUTHENTICATED!');
-        window.location.pathname !== '/login' &&
-          window.location.replace('/login');
-      }
-    }
+			if (notAuthenticated) {
+				console.log('NOT AUTHENTICATED!');
+				window.location.pathname !== '/login' &&
+					window.location.replace('/login');
+			}
+		}
 
-    if (networkError) {
-      console.log('ApolloClient networkError');
-      console.log(networkError);
-    }
-  },
+		if (networkError) {
+			console.log('ApolloClient networkError');
+			console.log(networkError);
+		}
+	},
 });
